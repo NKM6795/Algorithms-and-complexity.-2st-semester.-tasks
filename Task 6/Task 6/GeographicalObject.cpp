8da253f58365f486179ab1d9e1f6db6e5adfff31
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2019-03-03 19:46:43 +0200

    Created task 6 and added past tast data

---
+++ Task 6/Task 6/GeographicalObject.cpp
@@ -0,0 +1,67 @@
+#include "GeographicalObject.h"
+
+
+GeographicalObject::GeographicalObject()
+{
+
+}
+
+GeographicalObject::GeographicalObject(string name, long additionalInformation) : name(name), additionalInformation(additionalInformation)
+{
+
+}
+
+GeographicalObject::GeographicalObject(string name, long additionalInformation, vector<shared_ptr<GeographicalObject> > &ownedObjects) : name(name), additionalInformation(additionalInformation)
+{
+	setOwnedObjects(ownedObjects);
+}
+
+
+void GeographicalObject::setName(string name)
+{
+	this->name = name;
+}
+
+void GeographicalObject::setName(string &name)
+{
+	this->name = name;
+}
+
+string GeographicalObject::getName()
+{
+	return name;
+}
+
+
+void GeographicalObject::setAdditionalInformation(long additionalInformation)
+{
+	this->additionalInformation = additionalInformation;
+}
+
+void GeographicalObject::setAdditionalInformation(long &additionalInformation)
+{
+	this->additionalInformation = additionalInformation;
+}
+
+long GeographicalObject::getAdditionalInformation()
+{
+	return additionalInformation;
+}
+
+
+void GeographicalObject::setOwnedObjects(vector<shared_ptr<GeographicalObject> > &ownedObjects)
+{
+	for (int i = 0; i < int(ownedObjects.size()); ++i)
+	{
+		this->ownedObjects.addVertex(ownedObjects[i]);
+	}
+
+	cout << '\n';
+	this->ownedObjects.coutTree();
+}
+
+
+shared_ptr<GeographicalObject> GeographicalObject::getOwnedObject(long additionalInformation)
+{
+	return ownedObjects.getVertex(additionalInformation);
+}
---
+++ Task 6/Task 6/GeographicalObject.h
@@ -0,0 +1,39 @@
+#pragma once
+
+
+#include "Graph.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+
+class Graph;
+
+
+class GeographicalObject
+{
+	string name;
+	long additionalInformation;
+
+	Graph ownedObjects;
+
+public:
+	GeographicalObject();
+	GeographicalObject(string name, long additionalInformation);
+	GeographicalObject(string name, long additionalInformation, vector<shared_ptr<GeographicalObject> > &ownedObjects);
+
+	void setName(string name);
+	void setName(string &name);
+	string getName();
+
+	void setAdditionalInformation(long additionalInformation);
+	void setAdditionalInformation(long &additionalInformation);
+	long getAdditionalInformation();
+
+	void setOwnedObjects(vector<shared_ptr<GeographicalObject> > &ownedObjects);
+
+	shared_ptr<GeographicalObject> getOwnedObject(long additionalInformation);
+};
